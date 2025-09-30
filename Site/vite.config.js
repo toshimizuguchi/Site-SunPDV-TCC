797e392299ed9ad1fc56296d24654bf082563ad7
toshimizuguchi <toshi0123.mizuguchi@gmail.com>
2025-09-30 20:41:07 -0300

    colocando no github pages

--- Site/vite.config.js
+++ Site/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // isso garante que os caminhos funcionem no deploy
-});
-
+  base: "/SITE-SUNPDV-TCC/", // coloque o nome exato do seu repositório no GitHub
+})
