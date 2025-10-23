def692bdb4fecc5f0874451e4f11a8eca650d4eb
toshimizuguchi <toshi0123.mizuguchi@gmail.com>
2025-10-22 21:50:08 -0300

    atualizando a pagina sobre

--- Site/src/Components/PagSobr.jsx
+++ Site/src/Components/PagSobr.jsx
@@ -147,27 +147,36 @@ const PagSobr = () => {
         <div className="footer-content">
           <div className="footer-column">
             <h3>Sun PDV</h3>
-              <div className="footer-column">
-                 <p>Soluções completas para gestão de vendas e fluxo de caixa</p>
-            </div>
+            <p>Soluções completas para gestão de vendas e fluxo de caixa</p>
           </div>
 
           <div className="footer-column">
-            <h3>Suporte</h3>
+            <h3>Links Rápidos</h3>
             <ul className="footer-links">
-              <li><Link to="/contato">Contato</Link></li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/Sobre-Nos">Sobre Nós</Link></li>
+              <li><Link to="/Download">Download</Link></li>
             </ul>
           </div>
 
           <div className="footer-column">
-            <h3>Legal</h3>
+            <h3>Contato</h3>
             <ul className="footer-links">
-              <li><Link to="/privacidade">Política de Privacidade</Link></li>
-              <li><Link to="/termos">Termos de Uso</Link></li>
-              <li><Link to="/licenca">Licença</Link></li>
+              <li>suporte@sunpdv.com</li>
+              <li>(12) 3456-7890</li>
+              <li>WhatsApp: (12) 98765-4321</li>
             </ul>
           </div>
         </div>
+
+          <div className="footer-column">
+             <h3>Legal</h3>
+              <ul className="footer-links">
+                <li><Link to="/privacidade">Política de Privacidade</Link></li>
+                <li><Link to="/termos">Termos de Uso</Link></li>
+                <li><Link to="/licenca">Licença</Link></li>
+              </ul>
+          </div>
           
         <div className="copyright">
           <p>&copy; {new Date().getFullYear()} Sun PDV. Todos os direitos reservados.</p>
